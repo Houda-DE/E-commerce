905327297dab477c6cb589a66ae55bdeeefa3c26
Houda-DE <houdadebza8@gmail.com>
2022-12-23 17:20:47 +0100

    working on product card

--- frontend/components/ProductCard/index.tsx
+++ frontend/components/ProductCard/index.tsx
@@ -1,20 +1,40 @@
 import styles from './styles.module.css'
 import {AiOutlineHeart , AiFillHeart} from 'react-icons/ai'
+import { Rating } from '@mui/material'
+import Link from 'next/link'
+import {useState} from 'react'
 
 const ProductCard = ({name , imageUrl , price , reductionPercentage , addedToWishList , rating} : any) => {
+
+    const [isHovering , setIsHovering] = useState(false)
+
     return(
-        <div className={styles.container}>
-            <img src={imageUrl}/>
-            <div>
-                <div>{name}</div>
+        <div className={styles.container} onMouseOver={()=>setIsHovering(true)} onMouseOut={()=>setIsHovering(false)}>
+            <Link style={{textDecoration : 'none' , color :  'transparent'}} href='/'>
+                <img src={imageUrl}/>
+                <div className={styles.nameAndWishListContainer}>
+                    <div style={{color : '#222529'}}>{name}</div>
+                    {
+                        addedToWishList === false ? 
+                            <AiOutlineHeart style={{color : '#222529'}} title='Add to wishlist'/>
+                        :   
+                            <AiFillHeart style={{color : '#222529'}} title='Added to wishlist'/>
+                    }
+                </div>
+                <Rating name="read-only" defaultValue={rating/20} precision={0.1} readOnly/>
                 {
-                    addedToWishList === false ? 
-                        <AiOutlineHeart/>
-                    :   
-                        <AiFillHeart/>
+                    reductionPercentage !== 0 ? 
+                        <div className={styles.reductionContainer}>
+                            <div id={styles['oldPrice']}>${price}</div>
+                            <div>${price*reductionPercentage/100}</div>
+                        </div> 
+                    :
+                        <div className={styles.reductionContainer}>${price}</div>
                 }
-            </div>
-            <div>{price}</div>
+            </Link>
+            {
+                isHovering&&<button className={styles.button}>Add to cart</button>
+            }
         </div>
     )
 }
